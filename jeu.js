10db2f5445c3aa915c49ebe8c6a2474474c5942b
Marion BERNARD <marion.lautier.bernard@gmail.com>
2024-10-24 12:19:42 +0200

    controle du serpent

--- jeu.js
+++ jeu.js
@@ -2,7 +2,17 @@ const canvas = document.querySelector("#monCanvas")
 const ctx = canvas.getContext("2d")
 
 const direction = { x: 1, y: 0 }
-const snake = [{x:20, y:20}]
+const snake = [{ x: 20, y: 20 }]
+
+let intervalGame
+
+/***************************/
+/*******Lancer le jeu*******/
+/***************************/
+
+function startGame() {
+    intervalGame = setInterval(test, 100); 
+}
 
 /***************************/
 /****Dessiner le serpent****/
@@ -20,55 +30,79 @@ function drawSnake() {
 /****Déplacer le serpent****/
 /***************************/
 
+setInterval(test, 200)
 
-    setInterval(moveSnake,200)
-
-    function moveSnake() {
-        const newHead = {
-            x: snake[0].x + direction.x*20,
-            y: snake[0].y + direction.y*20
-        }
+function moveSnake() {
+    const newHead = {
+        x: snake[0].x + direction.x * 20,
+        y: snake[0].y + direction.y * 20
+    }
     snake.unshift(newHead)
     snake.pop()
     drawSnake()
-    }
+}
 
 /***************************/
 /***Contrôler le serpent****/
 /***************************/
 
-window.addEventListener('keydown', function(event) {
+window.addEventListener('keydown', function (event) {
+    if (direction.x === 0) {
+        switch (event.key) {
+            case 'ArrowRight':
+                direction.x = - direction.y
+                direction.y = 0
+                break
 
-    switch(event.key) {
+            case 'ArrowLeft':
+                direction.x = direction.y
+                direction.y = 0
+                break
+        }
+    } else {
+        switch (event.key) {
+            case 'ArrowRight':
+                direction.y = direction.x
+                direction.x = 0
+                break
 
-        case 'ArrowDown' : 
-            console.log('vers le bas')
-        break
-
-        case 'ArrowUp' : 
-            console.log('vers le haut')
-        break
-
-        case 'ArrowRight' : 
-            console.log('vers le haut')
-        break
-
-        case 'ArrowLeft' : 
-            console.log('vers le haut')
-        break
-
+            case 'ArrowLeft':
+                direction.y = - direction.x
+                direction.x = 0
+                break
+        }
     }
-   
+}
+)
+
+/*************************************/
+/****Voir si le serpent est en vie****/
+/*************************************/
+
+function checkDead() {
+    if (snake[0].x >= canvas.width || snake[0].x < 0 ||
+        snake[0].y >= canvas.height || snake[0].y < 0) {
+        gameOver();
+    }
+}
+
+const gameOverDiv = document.querySelector('#game-over')
+
+function gameOver () {
+    clearInterval(intervalGame)
+    gameOverDiv.classList.remove('hidden')
+}
+
+gameOverDiv.addEventListener('click', function() {
+    window.location.href = '/index.html'
 })
 
 
+function test() {
+    moveSnake()
+    checkDead()
+}
 
-window.onload = function() {
-    drawSnake();
-};
-
-
-
-
-
-
+window.onload = function () {
+    drawSnake()
+}
